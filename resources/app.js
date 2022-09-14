015508f652a683610d4c9a81f462054daebee77b
mk99995 <mejkorte@gmail.com>
2022-09-14 22:58:09 +0300

    added scroll menu

--- resources/app.js
+++ resources/app.js
@@ -1,3 +1,39 @@
+const slider = document.querySelector(".said-section");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+})
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+})
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+})
+
+slider.addEventListener("mousemove", (e) => {
+    if(!isDown) {
+        return;
+    }
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x- startX)*1.5;
+    slider.scrollLeft = scrollLeft - walk;
+})
+
+
+
+
+
+
+
+
 function formSubmit(e) {
     e.preventDefault() 
     let email = document.getElementById("email").value
